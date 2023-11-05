835218bc0f5f8acbceaff73fc8b59c790d22c4af
Rfer7114 <russel.workid@gmail.com>
2023-11-05 21:43:07 +0100

    updated gui.py file for adding items to a todo text file. fixed closed windows error with sg.WIN_CLOSED case function.

--- gui.py
+++ gui.py
@@ -1,10 +1,26 @@
+import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key='item')
 add_button = sg.Button("Add")
 
-window = sg.Window("TO-DO App", layout=[[label], [input_box, add_button]])
+window = sg.Window("TO-DO App", layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 13))
 
-window.read()
+while True:
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['item'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
