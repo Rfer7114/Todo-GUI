ec5c2c1a00a024108eb4278c9c94ce84853cbd66
Rfer7114 <russel.workid@gmail.com>
2023-11-05 16:29:50 +0100

    Added Add button to add to todos.txt

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath='todos.txt'):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Reads the todos text file and returns the
     items back to the calling client
     """
@@ -7,7 +9,7 @@ def get_todos(filepath='todos.txt'):
         return todos_local
 
 
-def write_todos(todos_arg, filepath='todos.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Writes the to-do new list back to the text file """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -1,10 +1,30 @@
+import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
+add_button2 = sg.Button("Show")
 
-window = sg.Window("TO-DO App", layout=[[label], [input_box, add_button]])
+window = sg.Window("TO-DO App",
+                   layout=[[label], [input_box, add_button], [add_button2]],
+                   font=('Acme', 15))
 
-window.read()
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case "Show":
+            todos = functions.get_todos()
+            print(value)
+
+        case sg.WIN_CLOSED:
+            break
 window.close()
