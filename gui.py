0b9faf0c7e2f4557868e84fc88e774b9d3876b00
Rfer7114 <russel.workid@gmail.com>
2023-11-08 11:53:36 +0100

    added try exception, theme and clock

--- gui.py
+++ gui.py
@@ -1,22 +1,24 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("Tealmono")
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter todo", key='item')
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.get_todos(), enable_events=True, key='list', size=(45, 10))
+list_box = sg.Listbox(values=functions.get_todos(), enable_events=True, key='list', size=(45, 15))
 edit_button = sg.Button("Edit")
 delete_button = sg.Button("Delete")
 exit_button = sg.Button("Exit")
 
-window = sg.Window("TO-DO App", layout=[[label], [input_box, add_button],
+window = sg.Window("TO-DO App", layout=[[clock],[label], [input_box, add_button],
                                         [list_box, edit_button, delete_button], [exit_button]],
                    font=('Helvetica', 13))
 
 while True:
-    event, values = window.read()
-    print(1, "EVENT:", event)
-    print(2, "VALUES:", values)
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime('%b %d, %Y %H:%M:%S'))
 
     match event:
         case "Add":
@@ -28,23 +30,29 @@ while True:
             window['item'].update(value="")
 
         case "Edit":
-            todo_to_edit = values['list'][0]
-            new_todo = values['item']
+            try:
+                todo_to_edit = values['list'][0]
+                new_todo = values['item']
 
-            todo_list = functions.get_todos()
-            index = todo_list.index(todo_to_edit)
-            todo_list[index] = new_todo
-            functions.write_todos(todo_list)
+                todo_list = functions.get_todos()
+                index = todo_list.index(todo_to_edit)
+                todo_list[index] = new_todo
+                functions.write_todos(todo_list)
 
-            window['list'].update(values=todo_list)
+                window['list'].update(values=todo_list)
+            except IndexError:
+                sg.popup("Select Item to edit first")
 
         case "Delete":
-            todo_to_delete = values['list'][0]
-            todo_list = functions.get_todos()
-            todo_list.remove(todo_to_delete)
-            functions.write_todos(todo_list)
-            window['list'].update(values=todo_list)
-            window['item'].update(value="")
+            try:
+                todo_to_delete = values['list'][0]
+                todo_list = functions.get_todos()
+                todo_list.remove(todo_to_delete)
+                functions.write_todos(todo_list)
+                window['list'].update(values=todo_list)
+                window['item'].update(value="")
+            except IndexError:
+                sg.popup("Select Item to delete first")
 
         case "list":
             window['item'].update(value=values['list'][0])
